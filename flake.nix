6d76a98cc7bd6f5b33d04e82ba8711cd0a4d6ef3
germanolives <germanolives@gmail.com>
2026-04-16 13:56:21 -0300

    migración a nix flake

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [ git gh python312 ];
+        shellHook = ''
+          echo "🚀 Minimalist Web Environment Activated"
+        '';
+      };
+    };
+}
--- shell.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.mkShell {
-  buildInputs = with pkgs; [ git gh python312 ];
-  shellHook = ''
-    export LANG=C.UTF-8
-    export LC_ALL=C.UTF-8
-    echo "🚀 Minimalist Web Environment Activated"
-  '';
-}
